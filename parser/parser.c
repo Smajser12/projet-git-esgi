e241094ef4ffc5e2204180fc6343b5955f3964e2
Smajser12 <nicola.brankovic@epita.fr>
2022-01-04 16:24:56 +0100

    add sh_split_line

--- parser/parser.c
+++ parser/parser.c
@@ -17,4 +17,38 @@ char* sh_read_line(FILE* f){
 
 	return line;
 }
+#define SH_TOK_BUFSIZE 64
+#define SH_TOK_DELIM " \t\r\n\a"
 
+char** sh_split_line(char *line){
+
+	int bufsize = SH_TOK_BUFSIZE, position = 0;
+	char **tokens = malloc(bufsize * sizeof(char*));
+	char *token, **tokens_backup;
+
+	if (!tokens) {
+		fprintf(stderr, "420sh: allocation error\n");
+		exit(EXIT_FAILURE);
+	}
+
+	token = strtok(line, SH_TOK_DELIM);
+	while (token != NULL) {
+		tokens[position] = token;
+		position++;
+
+		if (position >= bufsize) {
+			bufsize += SH_TOK_BUFSIZE;
+			tokens_backup = tokens;
+			tokens = realloc(tokens, bufsize * sizeof(char*));
+			if (!tokens) {
+				free(tokens_backup);
+				fprintf(stderr, "420sh: allocation error\n");
+				exit(EXIT_FAILURE);
+			}
+		}
+
+		token = strtok(NULL, SH_TOK_DELIM);
+	}
+	tokens[position] = NULL;
+	return tokens;
+}
